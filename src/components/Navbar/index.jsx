21c969c549cabd646fd27268c82032594d916458
James <james.hinton@live.co.uk>
2023-04-26 17:02:24 +0100

    style: navbar responsiveness improvements

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,15 +2,20 @@ import "./style.scss";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="container">
-        <ul className="links">
-          <li className="link">Shop</li>
-          <li className="link">Business and Government</li>
-          <li className="link">Education</li>
-        </ul>
-      </div>
-    </nav>
+    <div className="navbar-container">
+      <nav className="container">
+        <div className="navbar">
+          <ul className="links">
+            <li className="link">Shop</li>
+            <li className="link">Business and Government</li>
+            <li className="link">Education</li>
+          </ul>
+          <div className="navbar__login">
+            <button className="btn btn--secondary">Log in</button>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
