1ca816041e77b818036ec10a08a291d4dcd03de3
James <james.hinton@live.co.uk>
2023-04-26 12:17:08 +0100

    style: added custom fonts

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import "./style.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="links">
+        <li className="link">Shop</li>
+        <li className="link">Business and Government</li>
+        <li className="link">Education</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./Landing";
 import Contact from "./Contact";
+import Navbar from "../components/Navbar";
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
