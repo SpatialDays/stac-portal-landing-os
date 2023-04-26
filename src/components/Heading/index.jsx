29bc2b92a96eb75ed5a0c9164f3c1c427f27cb8a
James <james.hinton@live.co.uk>
2023-04-26 17:34:59 +0100

    feat: functionality to scroll to top of page

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,13 +1,29 @@
+import { useRef, useEffect } from "react";
 import Logo from "../../assets/images/logo.svg";
-
 import "./style.scss";
 
 const Heading = () => {
+  const logoRef = useRef(null);
+
+  useEffect(() => {
+    const logo = logoRef.current;
+
+    const handleClick = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    logo.addEventListener("click", handleClick);
+
+    return () => {
+      logo.removeEventListener("click", handleClick);
+    };
+  }, []);
+
   return (
     <div className="heading-container">
       <div className="container">
         <div className="heading">
-          <div className="heading__logo">
+          <div className="heading__logo" ref={logoRef}>
             <img src={Logo} alt="Logo" />
           </div>
           <div className="heading__login">
