f766eda7d8a3aae03fcaf9c7b497c16e3dde782c
James <james.hinton@live.co.uk>
2023-04-26 18:26:30 +0100

    feat: set primary colour from config

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App.jsx";
 import "./index.css";
+import config from "./site-config.json";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.documentElement.style.setProperty(
+    "--osweb-color-primary",
+    config.colours.primary
+  );
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
